01e682851756ab9c493ee99004df334598597926
Hans Alin <hansalin@gmail.com>
2023-11-15 22:33:42 +0100

    First code

--- trans_2.py
+++ trans_2.py
@@ -1,1 +1,10 @@
 # First line of code
+import math
+import os
+from tempfile import TemporaryDirectory
+from typing import Tuple
+
+import torch
+from torch import nn, Tensor
+from torch.nn import TransformerEncoder, TransformerEncoderLayer
+from torch.utils.data import dataset

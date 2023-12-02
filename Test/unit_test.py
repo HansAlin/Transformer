745f5773d85e1bb54be80ea7766b06c61cbdd029
Hans Alin <hansalin@gmail.com>
2023-12-02 12:04:20 +0100

    added asserts

--- Test/unit_test.py
+++ Test/unit_test.py
@@ -1,9 +1,12 @@
+import os
 import unittest
 from models.models_1 import TimeInputEmbeddings
 from dataHandler.datahandler import get_data, prepare_data
 
 
-data_path = '/home/halin/Master/Transformer/Test/data/mini_test_data.npy'
+path = os.getcwd()
+
+data_path = path + '/Test/data/mini_test_data.npy'
 x_train, x_test, y_train, y_test = get_data(path=data_path, test=True)
 
 train_loader, test_loader = prepare_data(x_train, x_test, y_train, y_test, 32)
@@ -16,14 +19,16 @@ for i in train_loader:
 
 
 class TestReturnDimenstions(unittest.TestCase):
-    def runBatchTest(self):
+    def test_runBatchTest(self):
         timeinput = TimeInputEmbeddings(d_model=512, embed_dim=1)
         output = timeinput(input_data)
         batch_size = output.size(dim=0)
         seq_len = output.size(dim=1)
         d_model = output.size(dim=2)
         print(batch_size)
-        self.assertEqual(batch_size, 200, "Incorrect size!")
+        self.assertEqual(batch_size, 32, "Incorrect size!")
+        self.assertEqual(seq_len, 100, "Incorrect sequence length!")
+        self.assertEqual(d_model, 512, "Incorrect dimension size!")
 
      
 

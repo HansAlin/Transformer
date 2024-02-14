dd02270e67db801e391fc992adcb76e19093cbe8
Hans Alin <hansalin@gmail.com>
2024-02-14 20:10:20 +0100

    bug fixing loading model and other states

--- main.py
+++ main.py
@@ -152,12 +152,12 @@ if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument('--start_model_num', help='Check for no interference', type=int)
 
-  parser.add_argument('--epochs', type=int, help='Default 100', default=1)
+  parser.add_argument('--epochs', type=int, help='Default 100', default=10)
   parser.add_argument('--test', type=bool, help='Default False', default=True)
   parser.add_argument('--cuda_device', type=int,help='Default 0', default=2)
   parser.add_argument('--config_number', type=int,help='Default 1', default=0)
-  parser.add_argument('--inherit_model', type=int,help='Default 18', default=201)
-  parser.add_argument('--retrain', type=bool,help='Default False', default=False)
+  parser.add_argument('--inherit_model', type=int,help='Default 18', default=1000)
+  parser.add_argument('--retrain', type=bool,help='Default False', default=True)
   
   args = parser.parse_args()
   main(args.start_model_num, args.epochs, args.test, args.cuda_device, args.config_number, args.inherit_model, args.retrain)
